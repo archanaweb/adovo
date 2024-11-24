fe163c5ccd8de969354c1dde755494ddd70f9fb0
archanaweb <thaarchana7jan@gmail.com>
2024-11-25 00:01:35 +0530

    updated

--- src/website/pages/home/index.jsx
+++ src/website/pages/home/index.jsx
@@ -69,7 +69,7 @@ const Home = () => {
           type="text" 
           name="userName"
           placeholder="Username" 
-          className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 bg-[#252837] border-gray-700 text-white" 
+          className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 bg-[#252837] border-gray-700 text-white mb-2" 
           value={formData?.userName}
           onChange={(e) => setFormData({...formData, userName: [e.target.value]})}
         />
