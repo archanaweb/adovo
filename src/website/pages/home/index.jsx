eb4acd818dc4646daa7dec4f4fcb99280d73624c
archanaweb <thaarchana7jan@gmail.com>
2024-11-27 01:15:18 +0530

    updated

--- src/website/pages/home/index.jsx
+++ src/website/pages/home/index.jsx
@@ -12,7 +12,7 @@ import trustpilot from "../../assest/images/trustpilot.png"
 import Faq from "../../components/Faq";
 import BaseUrl from "../../../Api/BaseUrl";
 import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate()
@@ -54,7 +54,7 @@ const Home = () => {
               <img src={trustpilot} alt="reviewsIcon" />
               </div>
             <div className="p-4 sm:p-6 md:p-6 bg-gray-700 rounded-lg shadow-md signup-form">
-      <h2 className="text-3xl font-bold mb-4 text-center text-gray-100">Sign in for free</h2>
+      <h2 className="text-2xl font-bold mb-4 text-center text-gray-100">Sign in for free</h2>
       <form onSubmit={handleSubmit} className="space-y-4">
 
       <div className="text-left">
@@ -86,22 +86,30 @@ const Home = () => {
           Start earning now
         </button>
       </form>
-      <div className="my-4 text-center text-gray-300 text-sm flex justify-between items-center gap-2"><div className="to-right-line"></div><span>OR SIGN UP WITH</span><div className="to-left-line"></div></div>
+      <div className="flex justify-between items-center pt-1">
+        <div className="flex justify-start items-center gap-1"> <input type="checkbox" id="rembPass" name="rembPass" />
+          <label for="rembPass" className="text-gray-400">Remember me</label></div>
+        <Link to='/' className="text-gray-400">Forgot password?</Link>
+      </div>
+      <div className="my-4 text-center text-gray-300 text-sm flex justify-between items-center gap-2"><div className="to-right-line"></div><span>OR SIGN IN WITH</span><div className="to-left-line"></div></div>
       <div className="flex gap-2 justify-center">
         {/* <button variant="outline" className="w-full bg-red-500 text-white hover:bg-red-700 mb-4">
             <FcGoogle className="w-5 h-5 mr-2" />
         </button> */}
-        <div className="">
-            <FcGoogle className="w-8 h-8 mr-2" />
+        <div className="cursor-pointer">
+            <FcGoogle className="w-6 h-6 mr-2" />
         </div>
-        <div className="">
-        <FaFacebook className="w-8 h-8 mr-2 text-white" /> 
+        <div className="cursor-pointer">
+        <FaFacebook className="w-6 h-6 mr-2 text-white" /> 
         </div>
         {/* <button variant="outline" className="w-full bg-blue-600 text-white hover:bg-blue-700">
             <FaFacebook className="w-5 h-5 mr-2" /> 
         </button> */}
       </div>
-      <p className="text-gray-400 text-sm mt-4 font-bold">
+      <div className="pt-2 account-btn">
+        <p>Don't have an acount yet? <Link to='/signup'>Register Now</Link></p>
+      </div>
+      <p className="text-gray-400 text-sm mt-2 font-bold">
         1264+ sign ups in the past 24 hours
       </p>
             </div>
