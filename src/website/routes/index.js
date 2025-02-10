b91ce82be1565572be3590a455da361e185c44c8
archanaweb <thaarchana7jan@gmail.com>
2025-02-10 22:07:48 +0530

    updated

--- src/website/routes/index.js
+++ src/website/routes/index.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider, useLocation } from "react-router-dom"
+import { BrowserRouter as Router, createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider, useLocation } from "react-router-dom"
 import Home from "../pages/home"
 import Signup from "../pages/signup"
 import VerifyOtp from "../pages/varifyOtp"
@@ -59,29 +59,31 @@ const LayoutDashboard = () => {
 const router = createBrowserRouter(
     createRoutesFromElements(
        <>
-        <Route path="/" element={<Layout />}>
-            <Route index element={<HomeNew />} loader={async () => await isAuthenticated()} /> 
-            <Route path="signup" element={<Signup />} loader={async () => await isAuthenticated()}> </Route>
-            <Route path="subuser/signup/:id" element={<SubUserSignup />}> </Route>
-            <Route path="verifyotp" element={<VerifyOtp />} loader={async () => await isAuthenticated()}> </Route>
-            <Route path="forgotpassword" element={<ForgotPassword />} loader={async () => await isAuthenticated()}> </Route>
-            <Route path="resetpassword" element={<ResetPassword />} loader={async () => await isAuthenticated()}> </Route>
-            <Route path="homeold" element={<Home />}> </Route>
-            <Route path="*" element={<h1 className="text-3xl">Page not found</h1>} />
+       <Router>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<HomeNew />} loader={async () => await isAuthenticated()} /> 
+                <Route path="signup" element={<Signup />} loader={async () => await isAuthenticated()}> </Route>
+                <Route path="subuser/signup/:id" element={<SubUserSignup />}> </Route>
+                <Route path="verifyotp" element={<VerifyOtp />} loader={async () => await isAuthenticated()}> </Route>
+                <Route path="forgotpassword" element={<ForgotPassword />} loader={async () => await isAuthenticated()}> </Route>
+                <Route path="resetpassword" element={<ResetPassword />} loader={async () => await isAuthenticated()}> </Route>
+                <Route path="homeold" element={<Home />}> </Route>
+                <Route path="*" element={<h1 className="text-3xl">Page not found</h1>} />
             </Route>
             <Route path="/" element={<LayoutDashboard />}>
-            <Route element={<Protected />}>
-                <Route path="dashboard" element={<UserDashboard/>}> </Route>
-                <Route path="alloffers" element={<AllOffers/>}> </Route>
-                <Route path="allsurveys" element={<AllSurvey/>}> </Route>
-                <Route path="survey/:id" element={<SurveyIframe />}> </Route>
-                <Route path="userprofile" element={<UserProfile/>}> </Route>
-                <Route path="cashout" element={<Cashout/>}> </Route>
-                <Route path="leaderboard" element={<LeaderBoard/>}> </Route>
-                <Route path="rewards" element={<Rewards/>}> </Route>
-                <Route path="affiliates" element={<Affiliates/>}> </Route>
+                <Route element={<Protected />}>
+                    <Route path="dashboard" element={<UserDashboard/>}> </Route>
+                    <Route path="alloffers" element={<AllOffers/>}> </Route>
+                    <Route path="allsurveys" element={<AllSurvey/>}> </Route>
+                    <Route path="survey/:id" element={<SurveyIframe />}> </Route>
+                    <Route path="userprofile" element={<UserProfile/>}> </Route>
+                    <Route path="cashout" element={<Cashout/>}> </Route>
+                    <Route path="leaderboard" element={<LeaderBoard/>}> </Route>
+                    <Route path="rewards" element={<Rewards/>}> </Route>
+                    <Route path="affiliates" element={<Affiliates/>}> </Route>
+                </Route>
             </Route>
-            </Route>
+        </Router>
         
         </>
     )
